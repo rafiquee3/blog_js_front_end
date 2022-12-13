bfc7b04952783e9aee3795350b8e92f0fec09021
Rafal Sokolowski <rafiquee.code@gmail.com>
2022-12-13 22:34:12 +0100

    Feat: css register form error notification

--- components/RegisterForm/RegisterForm.component.tsx
+++ components/RegisterForm/RegisterForm.component.tsx
@@ -29,8 +29,6 @@ const RegisterForm = () => {
   const style = css`
     display: flex;
     position: relative;
-    width: ${success ? '380px' : '500px'};
-    height: ${success ? '220px' : '400px'};
     border: 2px solid #166587;
     border-radius: 18px;
     overflow: hidden;
@@ -39,18 +37,17 @@ const RegisterForm = () => {
       display: flex;
       flex-direction: column;
       justify-content: center;
-      width: 40%;
       align-items: center;
       background: #166587;
+      padding: 30px;
     }
 
     div:nth-child(2) {
       display: flex;
       flex-direction: column;
-      justify-content: 'flex-start';
-      padding-top: ${success ? '50px' : '20px'};
+      justify-content: center;
       align-items: center;
-      width: 60%;
+      padding: 25px;
       background: #183D61;
 
       input {
@@ -60,7 +57,7 @@ const RegisterForm = () => {
 
       input[type="text"], input[type="password"] {
         height: 50px;
-        width: 70%;
+        width: 220px;
         margin: 5px;
         border: none;
         border-bottom: 1px solid #166587;
@@ -68,8 +65,6 @@ const RegisterForm = () => {
       }
 
       input[type="submit"], input[type="button"] {
-        position: absolute;
-        bottom: 12px;
         align-self: flex-end;
         height: 50px;
         color: #7FA2B3;
@@ -77,21 +72,12 @@ const RegisterForm = () => {
         border: 1px solid #166587;
         border-radius: 14px;
         padding: 12px 12px;
-        margin-right: 12px;
+        margin-top: 20px;
 
         &:hover {
           color: ${FontColor.GREEN};
           cursor: pointer;
           border-color: green;
-        }
-
-        & span {
-          position: absolute;
-          bottom: 0;
-          font-size: 0.7em;
-          margin: 1em;
-          color: ${FontColor.RED};
-          align-self: flex-start;
         }
       }
   
@@ -120,8 +106,8 @@ const RegisterForm = () => {
   `
   const styleCurrentField = css`
     position: absolute;
-    padding-bottom: ${success ? '22px' : '100px'};
     color: ${success ?  '#BABFBF' : FontColor.DEFAULT};
+    padding: 0px;
     bottom: 0;
   `
   const styleSuccess = css`
@@ -134,7 +120,14 @@ const RegisterForm = () => {
       filter: brightness(1.4);
     }
   `
-  const errField = {
+  const styleErrShowField = css`
+    position: absolute;
+    bottom: 0;
+    color: ${FontColor.RED};
+    padding: 5px;
+    font-size: 10px;
+  `
+  const styleErrField = {
     color: FontColor.RED, 
     borderBottom: `1px solid ${FontColor.RED}`
   }
@@ -256,7 +249,7 @@ const RegisterForm = () => {
               <input 
                 type="text" 
                 value={login} 
-                style={isValid('login') ? {} : errField}
+                style={isValid('login') ? {} : styleErrField}
                 onChange={e => setLogin(e.target.value)}
                 onFocus={() => setCurrentField('login')} 
                 name="login"
@@ -265,7 +258,7 @@ const RegisterForm = () => {
               <input 
                 type="password" 
                 value={password}
-                style={isValid('password') ? {} : errField} 
+                style={isValid('password') ? {} : styleErrField} 
                 onChange={e => setPassword(e.target.value)}
                 onFocus={() => setCurrentField('password')}  
                 name="password" 
@@ -274,7 +267,7 @@ const RegisterForm = () => {
               <input 
                 type="text" 
                 value={email} 
-                style={isValid('email') ? {} : errField} 
+                style={isValid('email') ? {} : styleErrField} 
                 onChange={e => setEmail(e.target.value)}
                 onFocus={() => setCurrentField('email')} 
                 name="email" 
@@ -324,6 +317,11 @@ const RegisterForm = () => {
             </div>
           </div>
   );
+  const ErrorMsg = (
+    <div className={styleErrShowField}>
+      {errorFields.map((elem: ErrorObj) => <p>{elem.field}: {elem.error}</p>)}
+    </div>
+  )
   useEffect(() => {
     if(errorFields.length) {
       if(prevLogin !== login) {
@@ -344,7 +342,117 @@ const RegisterForm = () => {
   return (
     <>
       {success ? SuccessMsg : Form}
+      {ErrorMsg}
     </>
   )
 }
 export default RegisterForm;
+
+// const style = css`
+// display: flex;
+// position: relative;
+// width: ${success ? '380px' : '500px'};
+// height: ${success ? '220px' : '400px'};
+// border: 2px solid #166587;
+// border-radius: 18px;
+// overflow: hidden;
+
+// div:nth-child(1) {
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   width: 40%;
+//   align-items: center;
+//   background: #166587;
+// }
+
+// div:nth-child(2) {
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: 'flex-start';
+//   padding-top: ${success ? '50px' : '20px'};
+//   align-items: center;
+//   width: 60%;
+//   background: #183D61;
+
+//   input {
+//     font-size: 1em;
+//     color: ${FontColor.DEFAULT};
+//   }
+
+//   input[type="text"], input[type="password"] {
+//     height: 50px;
+//     width: 70%;
+//     margin: 5px;
+//     border: none;
+//     border-bottom: 1px solid #166587;
+//     background: #183D61;
+//   }
+
+//   input[type="submit"], input[type="button"] {
+//     position: absolute;
+//     bottom: 12px;
+//     align-self: flex-end;
+//     height: 50px;
+//     color: #7FA2B3;
+//     background: #183D61;
+//     border: 1px solid #166587;
+//     border-radius: 14px;
+//     padding: 12px 12px;
+//     margin-right: 12px;
+
+//     &:hover {
+//       color: ${FontColor.GREEN};
+//       cursor: pointer;
+//       border-color: green;
+//     }
+
+//     & span {
+//       position: absolute;
+//       bottom: 0;
+//       font-size: 0.7em;
+//       margin: 1em;
+//       color: ${FontColor.RED};
+//       align-self: flex-start;
+//     }
+//   }
+
+//   input:focus-visible {
+//     outline: none;
+//     border-bottom: 1px solid ${FontColor.GREEN};
+//   }
+
+//   input::placeholder {
+//     font-size: 1em;
+//     opacity: .5;
+//     color: ${FontColor.GRAY};
+//   }
+// }
+
+// input:-webkit-autofill,
+// input:-webkit-autofill:hover, 
+// input:-webkit-autofill:focus, 
+// input:-webkit-autofill:active{
+//   -webkit-box-shadow: 0 0 0 30px #183D61 inset !important;
+// }
+// `
+// const styleImg = css`
+// opacity: ${success ? 0.5 : 0.4};
+// filter: ${success ? 'hue-rotate(340deg) saturate(130%) brightness(0.3)' : ''};
+// `
+// const styleCurrentField = css`
+// position: absolute;
+// padding-bottom: ${success ? '22px' : '100px'};
+// color: ${success ?  '#BABFBF' : FontColor.DEFAULT};
+// bottom: 0;
+// `
+// const styleSuccess = css`
+// color: ${FontColor.DEFAULT};
+// & p {
+//   text-align: center;
+// }
+// & b {
+//   color: #166587;
+//   filter: brightness(1.4);
+// }
+// `
